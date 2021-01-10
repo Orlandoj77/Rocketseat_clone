1ca12c4b84fd8aed6b99f32efe7f6119eccce29a
Orlandoj77 <orlandolaudes7@gmail.com>
2021-01-10 22:30:25 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/header/header";
 import Home from "./pages/homepage/home";
-
+import Footer from "./components/footer/foot";
 
 function App() {
     return (
         <div >
             <Header />
             <Home />
+            <Footer />
         </div>
     );
 }
